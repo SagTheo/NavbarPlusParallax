b5bdb27058bf7ce3a4c3f57d569d1e6d30b9b393
Theo <sagniez.theo@laposte.net>
2022-11-16 21:14:25 +0100

    organised files + created routing + finished working on responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import styles from './App.module.css';
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './screen/Home'
+import About from './screen/About'
+import Services from './screen/Services'
+import Contact from './screen/Contact'
+import Navbar from './components/Navbar';
 
 function App() {
-  const [displayNavbar, setDisplayNavbar] = useState(false)
-
   return (
-    <div className={styles.App}>
-      <div className={styles.navbarContainer}>
-        {
-          displayNavbar ?
-            (
-              <svg xmlns="http://www.w3.org/2000/svg" 
-                   width="32" 
-                   height="32" 
-                   fill="white" 
-                   className={`bi bi-x-lg ${styles.cross}`} 
-                   viewBox="0 0 16 16"
-                   onClick={() => setDisplayNavbar(false)}
-              >
-                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-              </svg>
-            )
-            :
-            (
-              <svg xmlns="http://www.w3.org/2000/svg" 
-                  width="32" 
-                  height="32" 
-                  fill="white" 
-                  className={`bi bi-list ${styles.menuIcon}`} 
-                  viewBox="0 0 16 16"
-                  onClick={() => setDisplayNavbar(true)}
-              >
-                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-              </svg>
-            )
-        }
+    <>
+      <Navbar />
 
-        <div className={`${styles.navbar} ${displayNavbar ? styles.responsiveNav : null}`}>
-          <div>Home</div>
-          <div>About</div>
-          <div>Services</div>
-          <div>Contact</div>
-        </div>
+      <div className={styles.App}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styles from '../css/Navbar.module.css'
+
+const Navbar = () => {
+    const [displayNavbar, setDisplayNavbar] = useState(false)
+    
+    return (
+        <div className={styles.navbarContainer}>
+            {
+            displayNavbar ?
+                (
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    width="32" 
+                    height="32" 
+                    fill="white" 
+                    className={`bi bi-x-lg ${styles.cross}`} 
+                    viewBox="0 0 16 16"
+                    onClick={() => setDisplayNavbar(false)}
+                >
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                </svg>
+                )
+                :
+                (
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    width="32" 
+                    height="32" 
+                    fill="white" 
+                    className={`bi bi-list ${styles.menuIcon}`} 
+                    viewBox="0 0 16 16"
+                    onClick={() => setDisplayNavbar(true)}
+                >
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                )
+            }
+
+            <div className={`${styles.navbar} ${displayNavbar ? styles.responsiveNav : null}`}>
+                <Link to='/' onClick={() => setDisplayNavbar(false)}>Home</Link>
+                <Link to='/about' onClick={() => setDisplayNavbar(false)}>About</Link>
+                <Link to='/services' onClick={() => setDisplayNavbar(false)}>Services</Link>
+                <Link to='/contact' onClick={() => setDisplayNavbar(false)}>Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
