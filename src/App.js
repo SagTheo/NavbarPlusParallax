710e81414940d01fde3c84ddd9a6b0eaf301feca
Theo <sagniez.theo@laposte.net>
2022-11-16 18:10:35 +0100

    created navbar + made it responsive + added animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { useState } from 'react'
 
 function App() {
+  const [displayNavbar, setDisplayNavbar] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.navbarContainer}>
+        {
+          displayNavbar ?
+            (
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                   width="32" 
+                   height="32" 
+                   fill="white" 
+                   className={`bi bi-x-lg ${styles.cross}`} 
+                   viewBox="0 0 16 16"
+                   onClick={() => setDisplayNavbar(false)}
+              >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+              </svg>
+            )
+            :
+            (
+              <svg xmlns="http://www.w3.org/2000/svg" 
+                  width="32" 
+                  height="32" 
+                  fill="white" 
+                  className={`bi bi-list ${styles.menuIcon}`} 
+                  viewBox="0 0 16 16"
+                  onClick={() => setDisplayNavbar(true)}
+              >
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            )
+        }
+
+        <div className={`${styles.navbar} ${displayNavbar ? styles.responsiveNav : null}`}>
+          <div>Home</div>
+          <div>About</div>
+          <div>Services</div>
+          <div>Contact</div>
+        </div>
+      </div>
     </div>
   );
 }
